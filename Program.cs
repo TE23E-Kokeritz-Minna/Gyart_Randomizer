1ee2f116d1464ca9423bb698bb4298dc4186846a
Minna Kökeritz <minna.kokeritz@elev.ga.ntig.se>
2026-02-11 14:15:28 +0100

    cleaned up  and final version

--- Program.cs
+++ Program.cs
@@ -7,8 +7,6 @@
 */
 
 //all pictures
-using System.Drawing;
-
 List<string> allPics = [
     "1. Solar Ascension (SP)",
     "2. Victory (SP)",
@@ -23,17 +21,17 @@ List<string> allPics = [
     "11. Lady Cunliffe(NGA)",
     "12. View la cava (NGA)",
     "13. Queen henirietta maria sir jeffrey hudson (NGA)",
-    "14. Philosopher (NGA)", 
+    "14. Philosopher (NGA)",
     "15. Roses (NGA)",
-    "16. Washington valley forge (NGA)", 
+    "16. Washington valley forge (NGA)",
     "17. Ripe (Ink: holm)",
-    "18. Troll attack (Ink: Higgins)", 
+    "18. Troll attack (Ink: Higgins)",
     "19. Bone (Ink: Hedge)",
     "20. Error (Ink: Birelmann)",
     "21. Holidays (Fan: P&C)",
-    "22. Elijah the rainbow dream dragon (Fan:Kayas )", 
+    "22. Elijah the rainbow dream dragon (Fan:Kayas )",
     "23. Slime girl (Fan: Remmade)",
-    "24. Sara- manga Style (Fan: Lilou)", 
+    "24. Sara- manga Style (Fan: Lilou)",
     "25. Concept game (Fan: Dadsky)",
 
     "AI.1. Solar Ascension ",
@@ -49,116 +47,57 @@ List<string> allPics = [
     "AI.11. Lady Cunliffe",
     "AI.12. View la cava ",
     "AI.13. Queen henirietta maria sir jeffrey hudson ",
-    "AI.14. Philosopher ", 
+    "AI.14. Philosopher ",
     "AI.15. Roses ",
-    "AI.16. Washington valley forge ", 
+    "AI.16. Washington valley forge ",
     "AI.17. Ripe ",
-    "AI.18. Troll attack ", 
+    "AI.18. Troll attack ",
     "AI.19. Bone ",
     "AI.20. Error ",
     "AI.21. Holidays ",
-    "AI.22. Elijah the rainbow dream dragon ", 
+    "AI.22. Elijah the rainbow dream dragon ",
     "AI.23. Slime girl ",
-    "AI.24. Sara - Manga Style", 
+    "AI.24. Sara - Manga Style",
     "AI.25. Concept art Game",
 ];
 
 
-//Console.WriteLine("Try: 9");
-//Console.WriteLine(allPics.Count);
-
-
-int countAllpics = allPics.Count;
-
 Console.WriteLine("Randomly generated order");
 
-/* for (var i = 0; i < countAllpics; i++)
-{
-    //Console.WriteLine(allPics[i]);
-    
-    int index = Random.Shared.Next(0, allPics.Count);
-   // Console.WriteLine("This is the index for next picture " + index + " left " + allPics.Count);
-    Console.WriteLine(allPics[index]);
-    allPics.RemoveAt(index);
-    //Console.WriteLine("this is how many left efter remove " + allPics.Count);
-} */
+//Counters for AI and Human pictures respectivly 
+int aiCount = 0;
+int humnaCount = 0;
 
-int aiCount = 0; 
-int humnaCount = 0; 
+// Loop until 40 pictures has been chosen
 for (var i = 0; i < 40; i++)
 {
-    //Console.WriteLine(allPics[i]);
-    Console.BackgroundColor = ConsoleColor.Black;
+    // Randomly chosen index 
     int index = Random.Shared.Next(0, allPics.Count);
-    // Console.WriteLine("This is the index for next picture " + index + " left " + allPics.Count);
-    if (allPics[index].Contains("AI")){
-        Console.ForegroundColor= ConsoleColor.Red;
-        aiCount ++; 
+
+    // Checks if the chosen picture index is AI or not 
+    if (allPics[index].Contains("AI"))
+    {
+        // Change colour for easier reading and advances the counter 
+        Console.ForegroundColor = ConsoleColor.Red;
+        aiCount++;
     }
-    else{
+    else
+    {
+        // Same as above 
         Console.ForegroundColor = ConsoleColor.Blue;
-        humnaCount ++; 
+        humnaCount++;
     }
+    // Writes the chosen picture
     Console.WriteLine(allPics[index]);
+    // Remove so it can't be chosen again 
     allPics.RemoveAt(index);
-    //Console.WriteLine("this is how many left efter remove " + allPics.Count);
 }
-    Console.ForegroundColor = ConsoleColor.White;
-    Console.WriteLine($"Total pictures: {humnaCount +aiCount}. human: {humnaCount}, Ai: {aiCount}"); 
+// Resets the text colour 
+Console.ForegroundColor = ConsoleColor.White;
+// Writes out the amount human and AI pictures and the total amount for dubble checking.
+Console.WriteLine($"Total pictures: {humnaCount + aiCount}. human: {humnaCount}, Ai: {aiCount}");
 
-/* 
-10. Basilica constantine doric colonnade (NGA)
-21. Holidays (Fan: P&C)
-9. Still Life with Artichokes and a parrot (NGA)
-AI.19. Bone 
-AI.23. Slime girl 
-2. Victory (SP)
-AI.24. Missing 1 human
-AI.16. Washington valley forge 
-3. Solarpunk lab (SP)
-AI.20. Error 
-22. Elijah the rainbow dream dragon (Fan:Kayas )
-AI.2. Victory 
-AI.6. How to build a solar punk city
-15. Roses (NGA)
-4. The floating village (SP)
-AI.1. Solar Ascension
-24. Missing 1 human(Fan: TBC)
-16. Washington valley forge (NGA)
-AI.12. View la cava
-AI.3. Solarpunk lab
-AI.8. Subak: water irrigation system
-AI.11. Lady Cunliffe
-7. A harvester of knowledge (SP)
-AI.13. Queen henirietta maria sir jeffrey hudson
-AI.9. Still Life with Artichokes and a parrot
-AI.15. Roses
-AI.5. Earth collage - utopia 2048
-19. Bone (Ink: Hedge)
-6. How to build a solar punk city (SP)
-25. Missing 2 human(Fan: TBC)
-5. Earth collage - utopia 2048 (SP)
-AI.17. Ripe
-17. Ripe (Ink: holm)
-12. View la cava (NGA)
-AI.25. Missing 2 human
-23. Slime girl (Fan: Remmade)
-8. Subak: water irrigation system (SP)
-1. Solar Ascension (SP)
-20. Error (Ink: Birelmann)
-AI.10. Basilica constantine doric colonnade
-11. Lady Cunliffe(NGA)
-AI.14. Philosopher
-AI.21. Holidays
-AI.22. Elijah the rainbow dream dragon
-AI.18. Troll attack
-14. Philosopher (NGA)
-AI.4. The floating village
-18. Troll attack (Ink: Higgins)
-AI.7. A harvester of knowledge
-13. Queen henirietta maria sir jeffrey hudson (NGA) */
-
-
+// The first output(when finsihed) having equal amount AI and Human pictures. Also the final order.
 /* 
 Randomly generated order
 14. Philosopher (NGA)
